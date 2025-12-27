8689890f5a03627b061469bd1c4c8d5c6b27a905
AhmedElrouby4 <ah3040861@gmail.com>
2025-12-27 18:23:54 +0200

    done

---
+++ s6/gen_pass.py
@@ -0,0 +1,10 @@
+import random
+
+def generate_password(length=8):
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*"
+    password = ""
+
+    for _ in range(length):
+        password += random.choice(characters)
+
+    return password
